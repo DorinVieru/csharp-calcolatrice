0e51a0d6d69377956ca1972e8281fcaf27dec915
DorinVieru <dorino.vieru@gmail.com>
2024-04-23 16:45:02 +0200

    UP: Create function for sum int & double

--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace csharp_calcolatrice
 {
+    public static class CalcoliHelper
+    {
+        // Somma di due numeri interi
+        public static int SommaInteri(int a, int b)
+        {
+            return a + b;
+        }
+
+        // IL FATTO DI UTILIZZARE LO STESSO METODO PER TIPI DI PARAMETRO DIVERSO INDICA CHE APPLICHIAMO IL PRINCIPIO DEL POLIMORFISMO, UNO DEI PRINCIPI DELLA PROGRAMMAZIONE AD OGGETTI
+        // Somma di due numeri double
+        public static double SommaDouble(double a, double b)
+        {
+            return a + b;
+        }
+
+        
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
